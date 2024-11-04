50afde04bc3e527c20ff03a11941c7182734922d
Meigi09 <rotimeigi@gmail.com>
2024-11-04 11:54:08 +0200

    Python Assignment

---
+++ user_input.py
@@ -0,0 +1,4 @@
+name = str(input("Enter your name: "))
+age = int(input("Enter your age: "))
+location = str(input("Enter your location: "))
+print("Hello "+ name +" , you are "+ age +" years old and live in "+ location)
